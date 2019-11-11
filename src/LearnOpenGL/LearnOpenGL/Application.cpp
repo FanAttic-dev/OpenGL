9d742ab236e31fa6f31a7a3cf5a872cc392b2a87
FanAttic <attic.zsiros@gmail.com>
2019-11-11 21:39:52 +0100

    no passphrase

--- src/LearnOpenGL/LearnOpenGL/Application.cpp
+++ src/LearnOpenGL/LearnOpenGL/Application.cpp
@@ -13,7 +13,6 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
 const unsigned int SCR_WIDTH = 2560;
 const unsigned int SCR_HEIGHT = 1440;
 
