2056a5dd930fdaeff9ec99bf5f211fe149c6be39
FanAttic <attic.zsiros@gmail.com>
2020-04-09 11:18:08 +0200

    Linearize depth

--- src/LearnOpenGL/LearnOpenGL/Shaders/no_texture.fs
+++ src/LearnOpenGL/LearnOpenGL/Shaders/no_texture.fs
@@ -1,7 +1,17 @@
 #version 430 core
-
 out vec4 FragColor;
 
-void main() {
-	FragColor = vec4(vec3(0.5), 1.0);
+float near = 0.1;
+float far = 10.0;
+
+float LinearizeDepth(float depth)
+{
+	float z = depth * 2.0 - 1.0; // back to NDC 
+	return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
+void main()
+{
+	float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
+	FragColor = vec4(vec3(depth), 1.0);
 }
