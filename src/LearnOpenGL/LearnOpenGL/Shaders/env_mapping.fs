49529858d8e21d3f4ca153de7be0e9413bb6e93b
FanAttic <attic.zsiros@gmail.com>
2020-04-15 18:30:34 +0200

    refraction - model - diamond

--- src/LearnOpenGL/LearnOpenGL/Shaders/env_mapping.fs
+++ src/LearnOpenGL/LearnOpenGL/Shaders/env_mapping.fs
@@ -7,9 +7,12 @@ in vec3 Normal;
 uniform vec3 eyePos;
 uniform samplerCube skybox;
 
+float ratio = 1.0 / 1.52;
+
 void main()
 {
 	vec3 I = normalize(FragPos - eyePos);
-	vec3 R = reflect(I, normalize(Normal));
+	//vec3 R = reflect(I, normalize(Normal));
+	vec3 R = refract(I, normalize(Normal), ratio);
 	FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
--- src/LearnOpenGL/LearnOpenGL/Shaders/model.fs
+++ src/LearnOpenGL/LearnOpenGL/Shaders/model.fs
@@ -16,13 +16,17 @@ uniform Material material;
 uniform samplerCube skybox;
 uniform vec3 eyePos;
 
+float ratio = 1.0 / 2.42;
+
 void main() 
 {
 	vec3 I = normalize(FragPos - eyePos);
-	vec3 R = reflect(I, normalize(Normal));
+	//vec3 R = reflect(I, normalize(Normal));
+	vec3 R = refract(I, normalize(Normal), ratio);
 	vec4 reflectionColor = vec4(texture(skybox, R).rgb, 1.0);
 
 	vec4 materialColor = texture(material.texture_diffuse1, TexCoords);
 
-	FragColor = (1 - materialColor.a) * materialColor + materialColor.a * reflectionColor;
+	//FragColor = (1 - materialColor.a) * materialColor + materialColor.a * reflectionColor;
+	FragColor = reflectionColor;
 }
