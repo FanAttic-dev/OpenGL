7e7775509f5b4df1cb3dba2ee55bf67e8064bd68
FanAttic <attic.zsiros@gmail.com>
2020-04-15 18:47:15 +0200

    model tweaked

--- src/LearnOpenGL/LearnOpenGL/Shaders/model.fs
+++ src/LearnOpenGL/LearnOpenGL/Shaders/model.fs
@@ -5,6 +5,7 @@ out vec4 FragColor;
 struct Material {
 	sampler2D texture_diffuse1;
 	sampler2D texture_specular1;
+	sampler2D texture_height1;
 	float shininess;
 };
 
@@ -21,12 +22,13 @@ float ratio = 1.0 / 1.33;
 void main() 
 {
 	vec3 I = normalize(FragPos - eyePos);
-	//vec3 R = reflect(I, normalize(Normal));
-	vec3 R = refract(I, normalize(Normal), ratio);
+	vec3 R = reflect(I, normalize(Normal));
+	//vec3 R = refract(I, normalize(Normal), ratio);
 	vec4 reflectionColor = vec4(texture(skybox, R).rgb, 1.0);
 
 	vec4 materialColor = texture(material.texture_diffuse1, TexCoords);
+	vec4 specularColor = texture(material.texture_specular1, TexCoords);
 
 	//FragColor = (1 - materialColor.a) * materialColor + materialColor.a * reflectionColor;
-	FragColor = reflectionColor;
+	FragColor = materialColor + mix(reflectionColor, specularColor, 0.3);
 }
